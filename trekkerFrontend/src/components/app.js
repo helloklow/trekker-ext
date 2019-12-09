b54bc0bc891bce52c9161724f2948bc56a0f23fd
kelypso <kklowlow@gmail.com>
2019-12-09 01:05:51 -0700

    stubbing out user page

--- trekkerFrontend/src/components/app.js
+++ trekkerFrontend/src/components/app.js
@@ -3,7 +3,8 @@ class App {
         this.adapter = new BaseAdapter()
         this.initBindingsAndEvents()
         // this.renderPage(new SignupPage(this.pageContainer, this.adapter))
-        this.renderPage(new LoginPage(this.pageContainer, this.adapter))
+        // this.renderPage(new LoginPage(this.pageContainer, this.adapter))
+        this.renderPage(new UserPage(this.pageContainer, this.adapter))
     }
 
     initBindingsAndEvents() {
@@ -11,6 +12,7 @@ class App {
         this.alertsContainer = document.querySelector('#alerts-container')
         this.navbarContainer = document.querySelector('#navbar-container')
         this.pageContainer = document.querySelector('#page-container')
+        this.parksContainer = document.querySelector('#parks-container')
     }
 
     // Render static HTML -> Update bindings and events -> Make any db calls -> Update page with dynamic HTML
