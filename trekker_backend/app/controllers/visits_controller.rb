11bec5f5e10f03a7858b58b4b08b379029119883
kelypso <kklowlow@gmail.com>
2019-12-12 14:00:14 -0700

    uncomment visits controller

--- trekker_backend/app/controllers/visits_controller.rb
+++ trekker_backend/app/controllers/visits_controller.rb
@@ -1,5 +1,5 @@
 class VisitsController < ApplicationController
-    # before_action :authenticate_user!
+    before_action :authenticate_user!
     
     def index
         visits = current_user.visits
